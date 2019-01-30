18af4365c96a8004b830a7351a60020651ccb802
王甜爽 <467637042@qq.com>
2019-01-30 16:54:21 +0800

    新增接口

--- weixin2/src/mixins/api.js
+++ weixin2/src/mixins/api.js
@@ -4,6 +4,10 @@ module.exports = {
   login: (data) => {return request('api/login', data, 'POST')},
   getUser: (data) => {return request('api/getUser', data)},
   getActivityList: (data) => {return request('api/activityList', data)},
-  addActivity: (data) => {return request('api/add/activity', data, 'POST')}
+  addActivity: (data) => {return request('api/add/activity', data, 'POST')},
+  getUserById: (data) => {return request('api/userinfo/id', data)},
+  getUnreadMsg: (data) => {return request('api/unread/msg', data)},
+  getMsg: (data) => {return request('api/getMsg', data)},
+  addMsg: (data) => {return request('api/add/msg', data), 'POST'}
 }
 
