a3a6472f0755ee6466140054ef59d8ee5e0be13f
王甜爽 <467637042@qq.com>
2019-11-26 19:22:54 +0800

    编码修改

--- max/wx/aescrypt.py
+++ max/wx/aescrypt.py
@@ -5,9 +5,9 @@ import datetime
 
 class EncryptAES():
   def __init__(self):
-    self.key = 'guokaigewangtian'
+    self.key = 'guokaigewangtian'.encode('utf-8')
     # self.iv = datetime.datetime.now().strftime('%Y-%m-%d-%H:%M')
-    self.iv = 'abcdefg123456789'
+    self.iv = 'abcdefg123456789'.encode('utf-8')
     self.mode = AES.MODE_CBC
 
   # 加密
@@ -19,7 +19,7 @@ class EncryptAES():
     text=self.bytePad(text,16)
     #add = length - (count % length)
     #text = text + ('\0' * add)
-
+    text = text.encode('utf-8')
     self.ciphertext = cryptor.encrypt(text)
     #因为AES加密时候得到的字符串不一定是ascii字符集的，输出到终端或者保存时候可能存在问题
     #所以这里统一把加密后的字符串转化为16进制字符串
@@ -75,7 +75,9 @@ class EncryptAES():
 if __name__ == '__main__':
     aes = EncryptAES()  # 初始化密钥
     e_result = aes.encrypt("123456")
+    print(e_result)
+    print(aes.decrypt(e_result))
     d_result = aes.decrypt("fe0269caf1dad12cb384218c8326ab8afcf3d36db4e72b873e075472c258543a810055977150b3d330de916a536f113113fe5386af7f6e9e23d4bb222aa1e9aa")
     # d_result = aes.decrypt(e_result)
-    # print(e_result)
-    # print(d_result)
+
+    print(d_result)
