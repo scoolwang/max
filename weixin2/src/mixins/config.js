b2712a7ae4ccee5594256c05445e0b0ec8e8ea5f
王甜爽 <467637042@qq.com>
2019-12-02 17:08:09 +0800

    supervisor

--- weixin2/src/mixins/config.js
+++ weixin2/src/mixins/config.js
@@ -3,6 +3,8 @@ module.exports = {
   secret: '30bb58edaaa9f9ab979c8e7d121d4bbe',
   // rootUrl: 'http://127.0.0.1:8000/'
   rootUrl: 'http://192.168.21.37:8000/'
+  // rootUrl: 'http://39.98.220.75:8000/'
+
   // rootUrl: 'http://47.94.244.143:8899/'
 
 }
