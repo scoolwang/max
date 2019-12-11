a2599b37b7fccede2f878a0b5dc820237f9b45fe
王甜爽 <467637042@qq.com>
2019-12-11 17:22:02 +0800

    修复bug

--- max/max/wsgi.py
+++ max/max/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
-import o
+import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'max.settings')
